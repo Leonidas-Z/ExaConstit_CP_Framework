be75a63cbf3a09c2fc5cf7aa4a46dd14b141e0a1
Leonidas Zisis <lzisis@purdue.edu>
2022-03-25 01:48:52 -0400

    Important changes

--- Exaopt_DEAP/ExaConstit_PostProcess.py
+++ Exaopt_DEAP/ExaConstit_PostProcess.py
@@ -1,12 +1,17 @@
+from matplotlib.pyplot import grid
 import numpy
 import pickle
-from SolutionPicker import BestSol
+import sys
+from sklearn.feature_extraction import grid_to_graph
 
-NOBJ=2
+
+# This script is using pymoo module visualization folder to show useful plots of the data
+
+
+NPOP = 52
 
 # Read file
-output="checkpoint_files/output_gen_11.pkl"
-
+output="checkpoint_files/output_gen_25.pkl"
 
 # Retrieve the state of the specified checkpoint
 with open(output,"rb+") as ckp_file:
@@ -18,8 +23,8 @@ pop_stress = ckp["pop_stress"]
 iter_tot = ckp["iter_tot"]
 last_gen = ckp["generation"]
 #NPOP = ckp["NPOP"]
-NPOP = 8
 
+print(sys.getsizeof(pop_stress))
 
 # ================================ Post Processing ===================================
 # Choose which generation you want to show in plots
@@ -29,6 +34,7 @@ pop_fit = numpy.array(pop_fit)
 
 
 # Find best solution
+from SolutionPicker import BestSol
 best_idx=BestSol(pop_fit, weights=[0.5, 0.5], normalize=False).EUDIST()
 
 
@@ -46,9 +52,10 @@ file=1
 plot3 = ExaPlots.MacroStressStrain(Exper_data = pop_stress[gen][best_idx][0][file], Simul_data = pop_stress[gen][best_idx][1][file], epsdot = strain_rate)
 
 from visualization.scatter import Scatter
-plot = Scatter(tight_layout=False)
+plot = Scatter(tight_layout=False, grid=True)
 plot.add(pop_fit, s=20)
 plot.add(pop_fit[best_idx], s=30, color="red")
+plot.show()
 
 from visualization.pcp import PCP
 plot = PCP(tight_layout=True)
@@ -58,12 +65,12 @@ plot.add(pop_fit[best_idx], linewidth=2, color="red")
 plot.show()
 
 from visualization.petal import Petal
-plot = Petal(bounds=[0, 0.02], tight_layout=True)
+plot = Petal(bounds=[0, 0.02], tight_layout=False)
 plot.add(pop_fit[best_idx])
 plot.show()
 #Put out of comments if we want to see all the individual fitnesses and not only the best
-plot = Petal(bounds=[0, 0.02], title=["Sol %s" % t for t in range(1,NPOP+1)], tight_layout=True)
-k = int(NPOP/2)
-plot.add(pop_fit[:k])
-plot.add(pop_fit[k:])
+plot = Petal(bounds=[0, 0.02], title=["Sol %s" % t for t in range(1,NPOP+1)], tight_layout=False)
+for k in range(1,NPOP+1):
+    if k%4==0:
+        plot.add(pop_fit[k-4:k])
 plot.show()
