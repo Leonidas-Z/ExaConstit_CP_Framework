ce4a49b560903894e34f64c274595b95006fcfdc
Leonidas Zisis <lzisis@purdue.edu>
2022-03-27 22:41:58 -0400

    Stress file added for visualization

--- Exaopt_DEAP/visualization/stress.py
+++ Exaopt_DEAP/visualization/stress.py
@@ -7,6 +7,8 @@ from visualization.staff.plot import Plot, set_if_none
 class Scatter(Plot):
 
     def __init__(self,
+                 epsdot=1e-3,
+                 custom_dt=None,
                  angle=(45, 45),
                  **kwargs):
         """
@@ -29,12 +31,17 @@ class Scatter(Plot):
         legend : {legend}
         tight_layout : {tight_layout}
         cmap : {cmap}
-
-        """
-
-        super().__init__(**kwargs)
-        self.angle = angle
+        """        
         
+        super().__init__(**kwargs)
+    
+        if custom_dt == None and epsdot == None:
+            print("one of the custom_dt and epsdot must not be None")
+        
+        self.angle = angle
+        self.epsdot = epsdot
+        self.custom_dt = custom_dt
+
 
     def _do(self):
 
@@ -42,6 +49,8 @@ class Scatter(Plot):
         is_2d = (self.n_dim == 2)
         is_3d = (self.n_dim == 3)
         more_than_3d = (self.n_dim > 3)
+
+        # find strain for each  
 
         # create the figure and axis objects
         if is_1d or is_2d:
@@ -52,7 +61,7 @@ class Scatter(Plot):
             self.init_figure(n_rows=self.n_dim, n_cols=self.n_dim)
 
         # now plot data points for each entry
-        for k, (F, kwargs) in enumerate(self.to_plot):
+        for k, (S_sim, kwargs) in enumerate(self.to_plot):
 
             # copy the arguments and set the default color
             _kwargs = kwargs.copy()
@@ -64,20 +73,20 @@ class Scatter(Plot):
                 del _kwargs["plot_type"]
 
             if is_1d:
-                F = np.column_stack([F, np.zeros(len(F))])
+                S_sim = np.column_stack([S_sim, np.zeros(len(S_sim))])
                 labels = self.get_labels() + [""]
 
-                self.plot(self.ax, _type, F, **_kwargs)
+                self.plot(self.ax, _type, S_sim, **_kwargs)
                 self.set_labels(self.ax, labels, False)
 
             elif is_2d:
-                self.plot(self.ax, _type, F, **_kwargs)
+                self.plot(self.ax, _type, S_sim, **_kwargs)
                 self.set_labels(self.ax, self.get_labels(), False)
 
             elif is_3d:
                 set_if_none(_kwargs, "alpha", 1.0)
 
-                self.plot(self.ax, _type, F, **_kwargs)
+                self.plot(self.ax, _type, S_sim, **_kwargs)
                 self.ax.xaxis.pane.fill = False
                 self.ax.yaxis.pane.fill = False
                 self.ax.zaxis.pane.fill = False
@@ -96,7 +105,7 @@ class Scatter(Plot):
                         ax = self.ax[i, j]
 
                         if i != j:
-                            self.plot(ax, _type, F[:, [i, j]], **_kwargs)
+                            self.plot(ax, _type, S_sim[:, [i, j]], **_kwargs)
                             self.set_labels(ax, [labels[i], labels[j]], is_3d)
                         else:
                             ax.set_xticks([])
@@ -107,22 +116,16 @@ class Scatter(Plot):
         return self
 
 
-    def plot(self, ax, _type, F, **kwargs):
+    def plot(self, ax, _type, S_sim, **kwargs):
         # kwargs includes all the addtional arguments for the matplotlib class (color, size, etc.)
-        is_3d = F.shape[1] == 3
+        is_3d = S_sim.shape[1] == 3
         if _type is None:
             _type = "scatter"
 
         if _type == "scatter":
-            if is_3d:
-                ax.scatter(F[:, 0], F[:, 1], F[:, 2], **kwargs)
-            else:
-                ax.scatter(F[:, 0], F[:, 1], **kwargs)
+                ax.scatter(S_sim[:, 0], S_sim[:, 1], **kwargs)
         else:
-            if is_3d:
-                ax.plot_trisurf(F[:, 0], F[:, 1], F[:, 2], **kwargs)
-            else:
-                ax.plot(F[:, 0], F[:, 1], **kwargs)
+                ax.plot(S_sim[:, 0], S_sim[:, 1], **kwargs)
 
     def set_labels(self, ax, labels, is_3d):
 
@@ -133,5 +136,24 @@ class Scatter(Plot):
         if is_3d:
             ax.set_zlabel(labels[2])
 
+    def find_strains(self, S_sim, epsdot, custom_dt):
 
-parse_doc_string(Scatter.__init__)
+        if custom_dt!=None:
+            time = np.loadtxt(self.custom_dt)
+
+        elif epsdot!=None:
+            nsteps = S_sim.shape[0]
+            eps = np.zeros(nsteps)
+            time = np.ones(nsteps)
+
+            for i in range(1, nsteps):
+                dtime = time[i]
+                if S_sim[i] - S_sim[i - 1] >= 0:
+                    eps[i] = eps[i - 1] + epsdot * dtime
+                else:
+                    eps[i] = eps[i - 1] - epsdot * dtime
+        return eps
+
+
+        
+
