2c5f90b2e6a7bea5ad705d298838090d0a2d083c
Leonidas Zisis <lzisis@purdue.edu>
2022-03-31 22:07:06 -0400

    Small refinements

--- Exaopt_DEAP/ExaConstit_NSGA3.py
+++ Exaopt_DEAP/ExaConstit_NSGA3.py
@@ -33,14 +33,14 @@ Also please look at the associated paper for the NSGAIII
 NOBJ = 2
 
 # Specify independent per experiment data file parameters (e.g. athermal parameters)
-IND_LOW = [0, 100,  50, 1500, 1e-5, 1e-3, 1e-4, 1e-5, 1e-6]
+IND_LOW = [150, 100,  50, 1500, 1e-5, 1e-3, 1e-4, 1e-5, 1e-6]
 IND_UP  = [200, 150, 100, 2500, 1e-3, 1e-1, 1e-2, 1e-3, 1e-4]
 # Specify dependent per experiment data file parameters (e.g. thermal parameters). If no dependent then set DEP_LOW = None, DEP_UP = None
 DEP_LOW = None #[1e-4, 1e-5, 1e-6]
 DEP_UP =  None #[1e-2, 1e-3, 1e-4]
 # Specify dependent parameters that will not be optimized and are different (dependent) per experiment data file (here is the temperature per file).
 # If no such parameters, set DEP_UNOPT = None
-DEP_UNOPT = [[270], [300]]
+DEP_UNOPT = [[270],[300]]
 
 
 # Final Bounds
